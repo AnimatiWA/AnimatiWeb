14a4bf8cfd01e173c07a6c0a1c0a1653c834440a
emir-argcoder <ea.samsam@gmail.com>
2025-05-13 01:10:46 -0300

    Se incorporan componentes de recuperación y cambio de password.

--- AnimatiWeb Angular/AnimatiWeb/src/app/app.routes.ts
+++ AnimatiWeb Angular/AnimatiWeb/src/app/app.routes.ts
@@ -17,6 +17,8 @@ import { LayoutComponent } from './admin/pages/layout/layout.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { AccesoAdminComponent } from './pages/auth/acceso-admin/acceso-admin.component';
 
+import { ChangePasswordComponent } from './pages/change-password/change-password.component';
+
 
 
 
@@ -35,6 +37,7 @@ export const routes: Routes = [
     {path: 'samplepage', title: 'Sample Page', component: SamplepageComponent},
     {path: 'agregarproductos', component:ProductsComponent},
     {path:'categoria', component:CategoriasComponent},
+    { path: 'change-password', component: ChangePasswordComponent },
     
     {path:"", redirectTo:"/", pathMatch:"full"},
     {
