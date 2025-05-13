b9a553f19dacbd7bfb9c898dbdf90f7a646f8b0a
Noemi Velazquez <velazqueznoemiandrea@gmail.com>
2025-05-13 08:14:07 -0300

    listar articulos destacados e Ir a la tienda

---
+++ AnimatiWeb Angular/AnimatiWeb/src/app/AppComponent.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavComponent } from './shared/nav/nav.component';
+
+
+
+
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [CommonModule, FooterComponent, NavComponent, RouterOutlet, HttpClientModule]
+})
+export class AppComponent {
+  title = 'AnimatiWeb';
+}
--- AnimatiWeb Angular/AnimatiWeb/src/app/pages/pagina-principal/pagina-principal.component.ts
+++ AnimatiWeb Angular/AnimatiWeb/src/app/pages/pagina-principal/pagina-principal.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit} from '@angular/core';
+import { HeaderComponent } from "../../shared/header/header.component";
+import { ProductService } from '../../services/productoServices/producto.service';
+import { HttpClient } from '@angular/common/http';
+import { Producto } from '../../interface/prductolista';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pagina-principal',
   standalone: true,
-  imports: [RouterLink],
   templateUrl: './pagina-principal.component.html',
-  styleUrl: './pagina-principal.component.css'
+  styleUrl: './pagina-principal.component.css',
+  imports: [CommonModule]
 })
-export class PaginaPrincipalComponent {
+export class PaginaPrincipalComponent implements OnInit {
+  listaProductos!: Producto[];
+  constructor(private http: HttpClient, private productoService: ProductService) { }
+
+  ngOnInit(): void {
+    this.getAllProductos();
+  }
+
+  getAllProductos() {
+    this.productoService.getProductos().subscribe(res => {
+      this.listaProductos = res;
+    });
+  }
+
+  agregarAlCarrito(producto: Producto) {
+
+    return this.productoService.agregarProductoCarrito(producto);
+
+  }
 
 }
