eabc9b93a2896fccd7c64a90dfb6796ed269f2ef
emir-argcoder <ea.samsam@gmail.com>
2025-05-14 01:45:34 -0300

    Se incorpora un componente modal para Playstore.

--- AnimatiWeb Angular/AnimatiWeb/src/app/shared/footer/footer.component.ts
+++ AnimatiWeb Angular/AnimatiWeb/src/app/shared/footer/footer.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { PromoModalComponent } from '../promo-modal/promo-modal.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+  constructor(private dialog: MatDialog) {}
 
+  openPromoModal() {
+    this.dialog.open(PromoModalComponent, { width: '400px' });
+  }
 }
