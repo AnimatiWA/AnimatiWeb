b48b202ca79629c1d47c185960eadc959df5e5a5
Gabriel Nieto De Jesus <92768772+GabrielNDJ@users.noreply.github.com>
2025-06-16 16:53:56 -0300

    Arreglo de nav carrito

--- AnimatiWeb Angular/AnimatiWeb/src/app/shared/nav/nav.component.ts
+++ AnimatiWeb Angular/AnimatiWeb/src/app/shared/nav/nav.component.ts
@@ -2,7 +2,8 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { LoginService } from '../../services/auth/login.service';
 import { Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Subscription } from 'rxjs';
+import { CarritoService } from '../../services/carritoServices/carrito.service';
 
 @Component({
   selector: 'app-nav',
@@ -16,28 +17,52 @@ export class NavComponent implements OnInit, OnDestroy {
   btnSecion:boolean = true;
   userLoginOn:boolean=false;
   userLoginOut:boolean=false;
+  cartItemsCount: number = 0;
+  private cartSubscription: Subscription | undefined;
   currentUserLoginOn: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-  constructor(private loginService:LoginService, private router:Router) {
+  
+  constructor(private loginService:LoginService, 
+              private router:Router,
+              private carritoService: CarritoService) {
     this.currentUserLoginOn=new BehaviorSubject<boolean>(sessionStorage.getItem("token")!=null); 
   }
   ngOnDestroy(): void {
-    this.userLoginOut
+
+    if (this.cartSubscription) {
+      this.cartSubscription.unsubscribe();
+    }
   }
 
   
 
   ngOnInit(): void {
-    this.loginService.userLoginOn.subscribe(
-      {
-        next:(userLoginOn) => {
-          this.userLoginOn=userLoginOn;
-        }
+    this.loginService.userLoginOn.subscribe({
+      next:(estado) => {
+        this.userLoginOn = estado;
         
+
+        if (estado) {
+          this.subscribeToCartChanges();
+        }
       }
-      
-    )
-    
+    });
+  }
   
+  private subscribeToCartChanges(): void {
+
+    this.cartSubscription = this.carritoService.cartItems$.subscribe(productos => {
+      if (productos && Array.isArray(productos)) {
+
+        this.cartItemsCount = productos.reduce((total, item) => {
+          return total + (item.Cantidad || 0);
+        }, 0);
+      } else {
+        this.cartItemsCount = 0;
+      }
+    });
+    
+
+    this.carritoService.actualizarProductosCarrito();
   }
 
 
