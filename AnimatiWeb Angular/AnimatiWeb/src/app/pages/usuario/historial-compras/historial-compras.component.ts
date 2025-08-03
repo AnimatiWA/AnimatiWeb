2b60ede93bd16aab9bb69dba96c84211f9ea74d5
Dali Palomeque <dalilapalomeque@mi.unc.edu.ar>
2025-08-02 22:43:40 -0300

    Se modifica el Historial de Compras para que se adapte al nuevo endpoint del back

--- AnimatiWeb Angular/AnimatiWeb/src/app/pages/usuario/historial-compras/historial-compras.component.ts
+++ AnimatiWeb Angular/AnimatiWeb/src/app/pages/usuario/historial-compras/historial-compras.component.ts
@@ -51,8 +51,6 @@ export class HistorialComprasComponent implements OnInit {
       day: '2-digit',
       month: '2-digit',
       year: 'numeric',
-      hour: '2-digit',
-      minute: '2-digit'
     });
   }
 }
